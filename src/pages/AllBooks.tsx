462d64ac2283b805abc93e91129ac38068574328
MD. Alamin <vijayalamin@gmail.com>
2023-07-15 22:38:14 +0600

    filters

--- src/pages/AllBooks.tsx
+++ src/pages/AllBooks.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { IBook } from '../interface/book.interface';
 import BookCard from '../components/BookCard';
+import UserLayout from '../layouts/UserLayout';
 
 const AllBooksPage: React.FC = () => {
     const [books, setBooks] = useState<IBook[]>([]);
@@ -42,42 +43,44 @@ const AllBooksPage: React.FC = () => {
     }, [searchTerm, genreFilter, publicationYearFilter, books]);
 
     return (
-        <div className="p-4">
-            <h1 className="text-2xl font-bold mb-4">All Books</h1>
-            <div className="flex space-x-2 mb-4">
-                <input
-                    type="text"
-                    placeholder="Search by title or author"
-                    className="input w-full"
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                />
+        <UserLayout>
+            <div className="p-4">
+                <h1 className="text-2xl font-bold mb-4">All Books</h1>
+                <div className="flex space-x-2 mb-4">
+                    <input
+                        type="text"
+                        placeholder="Search by title or author"
+                        className="input w-full"
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                    />
 
-                <select
-                    value={genreFilter}
-                    onChange={(e) => setGenreFilter(e.target.value)}
-                    className="select"
-                >
-                    <option value="">All Genres</option>
-                    <option value="Fiction">Fiction</option>
-                    <option value="Non-Fiction">Non-Fiction</option>
-                </select>
+                    <select
+                        value={genreFilter}
+                        onChange={(e) => setGenreFilter(e.target.value)}
+                        className="select"
+                    >
+                        <option value="">All Genres</option>
+                        <option value="Fiction">Fiction</option>
+                        <option value="Non-Fiction">Non-Fiction</option>
+                    </select>
 
-                <select
-                    value={publicationYearFilter}
-                    onChange={(e) => setPublicationYearFilter(e.target.value)}
-                    className="select"
-                >
-                    <option value="">All Years</option>
-                    <option value="2022">2022</option>
-                    <option value="2021">2021</option>
-                </select>
+                    <select
+                        value={publicationYearFilter}
+                        onChange={(e) => setPublicationYearFilter(e.target.value)}
+                        className="select"
+                    >
+                        <option value="">All Years</option>
+                        <option value="2022">2022</option>
+                        <option value="2021">2021</option>
+                    </select>
+                </div>
+
+                <ul className="space-y-4">
+                    {filteredBooks.map((book) => <BookCard book={book} key={book._id} />)}
+                </ul>
             </div>
-
-            <ul className="space-y-4">
-                {filteredBooks.map((book) => <BookCard book={book} key={book._id} />)}
-            </ul>
-        </div>
+        </UserLayout>
     );
 };
 
