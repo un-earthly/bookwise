08016cdf6b8ec3ba789273bdc972986cec775b11
MD. Alamin <vijayalamin@gmail.com>
2023-07-15 22:13:26 +0600

    login page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function Login() {
+    return (
+        <div className="min-h-screen hero-content flex-col">
+            <h1 className='lg:text-3xl font-semibold'>Please Login</h1>
+            <div className="flex-shrink-0 max-w-xl w-full shadow-2xl bg-base-100">
+                <div className="card-body">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="text" placeholder="email" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="text" placeholder="password" className="input input-bordered" />
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn btn-primary">Login</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
