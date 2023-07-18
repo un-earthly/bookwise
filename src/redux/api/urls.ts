a30cd03f579a35f55cfba7055f2fc80a8f031cc3
MD. Alamin <vijayalamin@gmail.com>
2023-07-18 11:31:49 +0600

    deploued backedn

--- src/redux/api/urls.ts
+++ src/redux/api/urls.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost/api/v1";
+export const BASE_URL = "https://backend-bookwise.vercel.app/api/v1";
 export const UserURL = BASE_URL + "/users"
 export const BookUrl = BASE_URL + "/book"
 export const wishListUrl = BASE_URL + "/wishlist"
